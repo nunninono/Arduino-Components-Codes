37e3b90ff198b9c5cd90247005203d9d669c13a5
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2025-03-18 00:02:58 -0300

    Add files via upload

---
+++ Pratica_02/Pratica_02.ino
@@ -0,0 +1,179 @@
+int led1 = 12;
+int led2 = 11;
+int led3 = 10;
+int led4 = 9;
+int led5 = 8;
+int led6 = 7;
+int led7 = 6;
+int led8 = 5;
+int led9 = 4;
+int led10 = 3;
+int t = 200;
+int trig = 2;
+int echo = 13;
+ 
+float distance;
+long duration;
+ 
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+ 
+void loop() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+
+  if (distance < 10) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 10 && distance <= 20) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 20 && distance <= 30) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 30 && distance <= 40) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 40 && distance <= 50) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 50 && distance <= 60) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 60 && distance <= 70) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, HIGH);
+    digitalWrite(led8, LOW);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 70 && distance <= 80) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, HIGH);
+    digitalWrite(led8, HIGH);
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+  }
+
+  else if (distance > 80 && distance <= 90) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, HIGH);
+    digitalWrite(led8, HIGH);
+    digitalWrite(led9, HIGH);
+    digitalWrite(led10, LOW);
+  }
+
+  else {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, HIGH);
+    digitalWrite(led8, HIGH);
+    digitalWrite(led9, HIGH);
+    digitalWrite(led10, HIGH);
+  }
+}
